1f2422ec6d5e915be586f13c157741f265751511
Caio Victor <caiovictor31@live.com>
2016-10-06 00:10:06 -0300

    Adding register functionality

--- log/views.py
+++ log/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
     return render(request, "home.html")
+
+def register(request):
+    return HttpResponse("... to do ...")
